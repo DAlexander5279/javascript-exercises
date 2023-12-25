2025af658bcfdffb77be70438847879a73b21a4b
DAlexander5279 <dalton.alexander@hotmail.com>
2023-12-25 18:24:37 -0500

    unit test 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+
+    let longWord = "";
+
+    if(times < 0)
+    {
+        return "ERROR"
+    }
+
+    for(let i = 0; i < times; i++)
+    {
+        longWord = longWord + word;
+    }
+    return longWord;
 
 };
 
