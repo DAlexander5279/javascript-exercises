b3df1bb04f463b7245e4baeb4ee7f0225b8ad3f9
DAlexander5279 <dalton.alexander@hotmail.com>
2023-12-26 18:01:30 -0500

    unit test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if(typeof num1 != "number" || typeof num2 != "number")
+    {
+        return "ERROR"; 
+    }
+        
+    if(num1 < 0 || num2 < 0)
+    {
+        return "ERROR"
+    }
+
+
+    if(num1 > num2)
+    {
+        max = num1
+        min = num2
+    }
+    else
+    {
+        max = num2
+        min = num1
+    }
+
+    let sum = 0;
+
+    for(let i =  min; i <= max; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
